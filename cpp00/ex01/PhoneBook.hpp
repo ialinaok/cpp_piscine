9d4bdaa1ed98d77c14e72dd9e17aaeb7dcdaf9d4
ialina <alinapielaszkiewicz@gmail.com>
2023-01-12 22:38:37 +0100

    go to sleep ali

---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apielasz <apielasz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 17:12:12 by apielasz          #+#    #+#             */
+/*   Updated: 2023/01/12 17:14:09 by apielasz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+class	PhoneBook {
+
+	public:
+		char	arr[8];
+
+		PhoneBook(void);
+		~PhoneBook(void);
+};
+
+#endif
