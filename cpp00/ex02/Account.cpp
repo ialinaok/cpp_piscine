9a17950adf60b14d08bcf03d6000c0591e18725f
ialina <alinapielaszkiewicz@gmail.com>
2023-01-18 19:54:27 +0100

    did the bonus ex, wohooo

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ialinaok <ialinaok@student.42.fr>          +#+  +:+       +#+        */
+/*   By: apielasz <apielasz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 17:01:36 by apielasz          #+#    #+#             */
-/*   Updated: 2023/01/17 23:22:10 by ialinaok         ###   ########.fr       */
+/*   Updated: 2023/01/18 19:28:19 by apielasz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,25 +43,23 @@ ahh also I guess from the log message I can see the format of the messages outpu
 Account::Account(int initial_deposit) :
 _accountIndex(this->_nbAccounts),
 _amount(initial_deposit),
-_nbDeposits(1),
+_nbDeposits(0),
 _nbWithdrawals(0) {
 
 	this->_nbAccounts++;
+	_totalAmount += initial_deposit;
 	t::_displayTimestamp();
-	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount;
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount;
 	std::cout << ";created" << std::endl;
 
 	return ;
 }
 
-// Account::Account(int const amounts[], size_t amounts_size) {
-
-// 	//idk honestly this makes just noooo sense
-// }
-
 Account::~Account(void) {
 
-	std::cout << "Destructor Account called" << std::endl;
+	t::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount;
+	std::cout << ";closed" << std::endl;
 
 	return ;
 }
@@ -89,13 +87,58 @@ int	Account::getNbWithdrawals(void) {
 void	Account::displayAccountsInfos(void) { //tbd
 
 	t::_displayTimestamp();
+	std::cout << "accounts:" << t::_nbAccounts << ";total:" << t::_totalAmount;
+	std::cout << ";deposits:" << t::_totalNbDeposits << ";withdrawals:" << t::_totalNbWithdrawals;
+	std::cout << std::endl;
 
-	for (int i = 0; i < t::getNbAccounts(); i++) {
+	return ;
+}
 
-		std::cout << "index:" << i << "amount:" << t::getTotalAmount();
-		std::cout << 
-		
+void	Account::makeDeposit(int deposit) {
+
+	this->_nbDeposits++;
+	t::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount;
+	std::cout << ";deposit:" << deposit;
+	this->_amount += deposit;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+	std::cout << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits;
+	std::cout << std::endl;
+
+	return ;
+}
+
+bool	Account::makeWithdrawal(int withdrawal) {
+
+	t::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount;
+	if (this->_amount - withdrawal >= 0) {
+
+		this->_amount -= withdrawal;
+		this->_nbWithdrawals++;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals++;
+		std::cout << ";withdrawal:" << withdrawal << ";amount:" << this->_amount;
+		std::cout << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+		return (true);
 	}
+	else
+		std::cout << ";withdrawal:refused" << std::endl;
+	return (false);
+}
+
+int	Account::checkAmount(void) const {
+
+	return (this->_amount);
+}
+
+void	Account::displayStatus(void) const {
+
+	t::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount;
+	std::cout << ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals;
+	std::cout << std::endl;
 }
 
 void	Account::_displayTimestamp(void) {
