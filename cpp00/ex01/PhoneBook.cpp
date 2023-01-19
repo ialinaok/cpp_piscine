45ad92b26a0e6b9e2d95304464ff81452e2179d3
ialinaok <alinapielaszkiewicz@gmail.com>
2023-01-19 21:54:57 +0100

    finished

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,13 +6,16 @@
 /*   By: ialinaok <ialinaok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 17:11:45 by apielasz          #+#    #+#             */
-/*   Updated: 2023/01/16 17:51:50 by ialinaok         ###   ########.fr       */
+/*   Updated: 2023/01/19 21:49:09 by ialinaok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 #include "PhoneBook.hpp"
+
+int		PhoneBook::_nb_contacts_added = 0;
+bool	PhoneBook::_is_full = false;
 
 PhoneBook::PhoneBook(void) {
 
@@ -28,15 +31,166 @@ PhoneBook::~PhoneBook(void) {
 	return ;
 }
 
-int	PhoneBook::add_contact_to_list(Contact new_contact, int index) {
+int	PhoneBook::add_contact_to_array(Contact new_contact, int index) {
 
 	this->_list_of_contacts[index] = new_contact;
 	std::cout << "Contact #" << index << " added" << std::endl;
-
+	_nb_contacts_added++;
+	if (_nb_contacts_added == 8)
+		_is_full = true;
 	return (0);
 }
 
 Contact	PhoneBook::get_contact(int index) const {
 
 	return (this->_list_of_contacts[index]);
+}
+
+int	PhoneBook::get_nb_contacts_added(void) const {
+
+	return (_nb_contacts_added);
+}
+
+bool	PhoneBook::get_is_full(void) const {
+
+	return (_is_full);
+}
+
+// this function will return true only if there's whspc and one single digit in input.
+// pre-check for atoi
+bool	PhoneBook::valid_index(std::string str) const {
+
+	int i = 0;
+
+	while (std::isspace(str.c_str()[i]) != 0)
+		i++;
+	if (std::isdigit(str.c_str()[i]) == 0)
+		return (false);
+	else
+		i++;
+	while (std::isspace(str.c_str()[i]) != 0)
+		i++;
+	if (str.c_str()[i] != '\0')
+		return (false);
+	return (true);
+}
+
+void	PhoneBook::get_contact_data(PhoneBook *the_book, int index) {
+
+	Contact		contact;
+	std::string	temp;
+
+	std::cout << "Enter first name" << std::endl << ": ";
+	std::getline(std::cin >> std::ws, temp);
+	contact.set_first_name(temp);
+	std::cout << "Enter last name" << std::endl << ": ";
+	std::getline(std::cin >> std::ws, temp);
+	contact.set_last_name(temp);
+	std::cout << "Enter nickname" << std::endl << ": ";
+	std::getline(std::cin >> std::ws, temp);
+	contact.set_nickname(temp);
+	while (42) {
+		
+		int	i;
+
+		std::cout << "Enter phone number (without spaces)" << std::endl << ": ";
+		std::getline(std::cin >> std::ws, temp);
+		for (i = 0; i < (int) temp.length(); i++) {
+			if (std::isdigit(temp[i]) == 0 || std::isspace(temp[i]) != 0) {
+
+				std::cout << RED << "Error: Please, use only digits while entering the number!" << WH << std::endl;
+				break ;
+			}
+		}
+		if (i == (int) temp.length())
+			break ;
+	}
+	contact.set_phone_number(temp);
+	std::cout << "Enter darkest secret" << std::endl << ": ";
+	std::getline(std::cin >> std::ws, temp);
+	contact.set_darkest_secret(temp);
+
+	the_book->add_contact_to_array(contact, index);
+
+	return ;
+}
+
+void	PhoneBook::search_contacts(PhoneBook *the_book) const {
+
+	int	index = 0;
+	std::string	i;
+
+	the_book->print_table(the_book);
+	if (the_book->get_nb_contacts_added() == 0) {
+
+		std::cout << std::endl << RED << "PhoneBook is empty - there's no contact info to display. Try adding a contact first!" << WH << std::endl;
+		return ;
+	}
+	if (the_book->get_is_full() == false)
+		std::cout << std::endl << YELL << "Please, choose a contact to display [index #0 - #" << the_book->get_nb_contacts_added() - 1 << "]";
+	else
+		std::cout << std::endl << YELL << "Please, choose a contact to display [index #0 - #7]";
+	std::cout << WH << std::endl << ": ";
+	std::getline(std::cin >> std::ws, i);
+	while (the_book->valid_index(i) == false) {
+
+		std::cout << RED << "This is not a valid index. Please only type one digit.";
+		std::cout << WH << std::endl << ": ";
+		std::getline(std::cin >> std::ws, i);
+	}
+	if (the_book->valid_index(i) == true)
+		index = std::atoi(i.c_str());
+	while (((index < 0 || index > the_book->get_nb_contacts_added() - 1) && the_book->get_is_full() == false) || \
+		((index < 0 || index > 7) && the_book->get_is_full() == true)) {
+
+		if (the_book->get_is_full() == false)
+			std::cout << RED << "Please, choose index from #0 to #" << the_book->get_nb_contacts_added() - 1;
+		else
+			std::cout << RED << "Please, choose index from #0 to #7";
+		std::cout << WH << std::endl << ": ";
+		std::getline(std::cin >> std::ws, i);
+		while (the_book->valid_index(i) == false) {
+
+			std::cout << RED << "This is not a valid index. Please only type one digit.";
+			std::cout << WH << std::endl << ": ";
+			std::getline(std::cin >> std::ws, i);
+		}
+		if (the_book->valid_index(i) == true)
+			index = std::atoi(i.c_str());
+	}
+	std::cout << std::endl << PU << "First name: " << WH << the_book->get_contact(index).get_first_name() << std::endl;
+	std::cout << PU << "Last name: " << WH << the_book->get_contact(index).get_last_name() << std::endl;
+	std::cout << PU << "Nickname: " << WH << the_book->get_contact(index).get_nickname() << std::endl;
+	std::cout << PU << "Phone number : " << WH << the_book->get_contact(index).get_phone_number() << std::endl;
+	std::cout << PU << "Darkest secret: " << WH << the_book->get_contact(index).get_darkest_secret() << std::endl;
+}
+
+void	PhoneBook::print_table(PhoneBook *the_book) const {
+
+	int	index = 0;
+
+	std::cout << std::setw(10) << std::right << "Index" << "|";
+	std::cout << std::setw(10) << std::right << "First name" << "|";
+	std::cout << std::setw(10) << std::right << "Last name" << "|";
+	std::cout << std::setw(10) << std::right << "Nickname" << std::endl;
+	while (index < 8) {
+
+		std::cout << std::setw(10) << std::setfill(' ') << index << "|";
+		if (the_book->get_contact(index).get_first_name().length() >= 10)
+			std::cout << std::setw(9) << std::setfill(' ') << the_book->get_contact(index).get_first_name().substr(0, 9) << ".|";
+		else
+			std::cout << std::setw(10) << std::setfill(' ') << the_book->get_contact(index).get_first_name() << "|";
+		if (the_book->get_contact(index).get_last_name().length() >= 10)
+			std::cout << std::setw(9) << std::setfill('.') << the_book->get_contact(index).get_last_name().substr(0, 9) << ".|";
+		else
+			std::cout << std::setw(10) << std::setfill(' ') << the_book->get_contact(index).get_last_name() << "|";
+		if (the_book->get_contact(index).get_nickname().length() >= 10)
+			std::cout << std::setw(9) << std::setfill(' ') << the_book->get_contact(index).get_nickname().substr(0, 9) << "." << std::endl;
+		else
+			std::cout << std::setw(10) << std::setfill(' ') << the_book->get_contact(index).get_nickname() << std::endl;
+
+		index++;
+	}
+
+	return ;
 }
--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: ialinaok <ialinaok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 17:12:12 by apielasz          #+#    #+#             */
-/*   Updated: 2023/01/15 17:49:43 by ialinaok         ###   ########.fr       */
+/*   Updated: 2023/01/19 21:51:48 by ialinaok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,22 @@
 # define PHONEBOOK_H
 
 #include "Contact.hpp"
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstring>
+
+// *** COLORS ***//
+# define BLANK "\e[0m"
+# define BLACK "\033[0;30m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELL "\033[0;33m"
+# define BL "\033[0;34m"
+# define PU "\033[0;35m"
+# define CY "\033[0;36m"
+# define WH "\033[0;37m"
+# define D "\033[0m"
 
 class	PhoneBook {
 
@@ -21,11 +37,20 @@ class	PhoneBook {
 		PhoneBook(void);
 		~PhoneBook(void);
 
-		int		add_contact_to_list(Contact new_contact, int index);
+		int		add_contact_to_array(Contact new_contact, int index);
 		Contact	get_contact(int index) const;
+		int		get_nb_contacts_added(void) const;
+		bool	get_is_full(void) const;
+		bool	valid_index(std::string str) const;
+		void	get_contact_data(PhoneBook *the_book, int index);
+		void	search_contacts(PhoneBook *the_book) const;
+		void	print_table(PhoneBook *the_book) const;
 
 	private:
-		Contact	_list_of_contacts[8]; //check if you can use just number or if you need to declare a const
+		static int	_nb_contacts_added;
+		static bool	_is_full;
+
+		Contact	_list_of_contacts[8];
 };
 
 #endif
