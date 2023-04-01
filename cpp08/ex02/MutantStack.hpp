5eba428ab7d93f0c99b23f1de5752765cab52acc
ialina <alinapielaszkiewicz@gmail.com>
2023-04-01 20:27:25 +0200

    mutant hehe

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apielasz <apielasz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/01 17:56:04 by apielasz          #+#    #+#             */
+/*   Updated: 2023/04/01 20:05:55 by apielasz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANT_STACK_H
+# define MUTANT_STACK_H
+
+template <typename T>
+class	MutantStack : public std::stack {
+
+	public:
+		
+}
+
+#endif
