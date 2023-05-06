9bf3a191d5c3813579125bbb11e3044e20d4a094
ialina <alinapielaszkiewicz@gmail.com>
2023-05-06 18:30:56 +0200

    wip

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apielasz <apielasz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 16:45:12 by apielasz          #+#    #+#             */
+/*   Updated: 2023/05/06 16:52:06 by apielasz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	use positive integer sequence as argument
+	./PmergeMe 3 5 9 7 4
+	- input checking - convert to numbers and check if they're bigger than 0 and smaller than MAXINT
+		but MAX_UINT, because we only accept ints anyway
+		check number of args if it's at least 3
+
+	
+	
